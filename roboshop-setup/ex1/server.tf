335a44e1c2d3326fff30d858ba18e33c7fa2c3d1
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-02-28 13:04:05 +0530

    roboshop-setup in terraform

---
+++ roboshop-setup/ex1/server.tf
@@ -0,0 +1,169 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "frontend-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "cart-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "catalogue-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "user" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "user-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "shipping-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "payment" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "payment-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "mongodb-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "mysql-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "redis-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z04486134O6KRB187HQS"
+  name    = "rabbitmq-dev.roboshop.internal"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
