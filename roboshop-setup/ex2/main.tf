6aac72d7304c756b38cd8f7b0aeb4e5b6124a452
Sudheer4reddy <reddigarisudheer447@gmail.com>
2023-03-03 15:18:21 +0530

    roboshop setup in ansible

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["132802166149"]
+}
+
+resource "aws_instance" "frontend" {
+  count                  = 5
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0a200456bd387da98"]
+  tags = {
+    Name = "frontend"
+  }
+}
